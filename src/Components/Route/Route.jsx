86964a7b8ae5d0d1d57500a750d69dc355bc2659
Salman Rahaman Jony <jonyislamcmt@gmail.com>
2023-09-25 15:42:11 +0600

    create route with main.jsx

---
+++ src/Components/Route/Route.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const route = createBrowserRouter([
+    {
+        path : '/',
+        element : <h2>Hello router</h2>,
+    }
+])
+
+export default route;
